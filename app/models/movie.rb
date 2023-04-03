9626d1ddd98a5f1a883c4b5a5077811350dd228f
nnti3n <thienphong215@gmail.com>
2023-04-03 21:30:40 +0700

    Add user movie relation

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,5 @@
 class Movie < ApplicationRecord
   validates :title, :youtube_id, presence: true
+
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_01_083820) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_03_132813) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_01_083820) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_movies_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -35,4 +37,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_01_083820) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "movies", "users"
 end
